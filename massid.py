955770552e4726c14c909913d7b8395eea7700ed
triineliskuum <triin-elis@ut.ee>
2024-11-09 17:08:27 +0200

    Update massid.py

---
+++ massid.py
@@ -0,0 +1,47 @@
+def kg_g(kg):
+    return kg * 1000
+
+def g_kg(g):
+    return g / 1000
+
+def kg_mg(kg):
+    return kg * 1000000
+
+def mg_kg(mg):
+    return mg / 1000000
+
+def mg_g(mg):
+    return mg / 1000
+
+def g_mg(g):
+    return g * 1000
+
+def lb_g(lb):
+    return lb * 453.59237
+
+def g_lb(g):
+    return g * 0.00220462262
+
+def oz_g(oz):
+    return oz * 28.3495231
+
+def g_oz(g):
+    return g * 0.035274
+
+def kg_lb(kg):
+    return kg * 2.205
+
+def lb_kg(lb):
+    return lb * 0.454
+
+def kg_oz(kg):
+    return kg * 35.2739619
+
+def oz_kg(oz):
+    return oz * 0.0283495231
+
+def lb_oz(lb):
+    return lb * 16
+
+def oz_lb(oz):
+    return oz * 0.0625
