770ad8d817029587e2e367b9a328a1562b63404a
Karel Konga <kongakarel@gmail.com>
2024-11-09 22:34:18 +0200

    mahud plus mahu teisendus

--- mahud.py
+++ mahud.py
@@ -1,10 +1,40 @@
 # Metric to Metric
 
+def l_to_dl(l):
+    return l * 10
+
+def l_to_cl(l):
+    return l * 100
+
 def l_to_ml(l):
     return l * 1000
 
+def dl_to_l(dl):
+    return dl / 10
+
+def dl_to_cl(dl):
+    return dl * 10
+
+def dl_to_ml(dl):
+    return dl * 100
+
+def cl_to_l(cl):
+    return cl / 100
+
+def cl_to_dl(cl):
+    return cl / 10
+
+def cl_to_ml(cl):
+    return cl * 10
+
 def ml_to_l(ml):
     return ml / 1000
+
+def ml_to_dl(ml):
+    return ml / 100
+
+def ml_to_cl(ml):
+    return ml / 10
 
 # Metric to Imperial
 
@@ -199,20 +229,8 @@ def imperial_pints_to_us_pints(imp_pt):
 def imperial_fl_oz_to_us_fl_oz(imp_oz):
     return imp_oz * 1.04084
 
-def imperial_gallons_to_us_gallons(imp_gal):
-    return imp_gal * 1.20095
-
-def imperial_quarts_to_us_quarts(imp_qt):
-    return imp_qt * 1.20095
-
-def imperial_pints_to_us_pints(imp_pt):
-    return imp_pt * 1.20095
-
 def imperial_cups_to_us_cups(imp_cup):
     return imp_cup * 1.20095
-
-def imperial_fl_oz_to_us_fl_oz(imp_oz):
-    return imp_oz * 1.04084
 
 def imperial_tablespoons_to_us_tablespoons(imp_tbsp):
     return imp_tbsp * 1.20095
@@ -235,20 +253,8 @@ def us_pints_to_imperial_pints(us_pt):
 def us_fl_oz_to_imperial_fl_oz(us_oz):
     return us_oz * 0.96076
 
-def us_gallons_to_imperial_gallons(us_gal):
-    return us_gal * 0.832674
-
-def us_quarts_to_imperial_quarts(us_qt):
-    return us_qt * 0.832674
-
-def us_pints_to_imperial_pints(us_pt):
-    return us_pt * 0.832674
-
 def us_cups_to_imperial_cups(us_cup):
     return us_cup * 0.832674
-
-def us_fl_oz_to_imperial_fl_oz(us_oz):
-    return us_oz * 0.96076
 
 def us_tablespoons_to_imperial_tablespoons(us_tbsp):
     return us_tbsp * 0.832674
