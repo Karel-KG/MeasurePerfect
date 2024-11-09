db3bd4749b770e1fbaea912f76577fdbfbb41439
Karel Konga <kongakarel@gmail.com>
2024-11-09 20:57:52 +0200

    mahud

---
+++ mahud.py
@@ -0,0 +1,335 @@
+# Metric to Metric
+
+def l_to_ml(l):
+    return l * 1000
+
+def ml_to_l(ml):
+    return ml / 1000
+
+# Metric to Imperial
+
+def l_to_imperial_gallons(l):
+    return l / 4.54609
+
+def l_to_imperial_quarts(l):
+    return l / 1.13652
+
+def l_to_imperial_pints(l):
+    return l / 0.568261
+
+def l_to_imperial_fl_oz(l):
+    return l / 0.0284131
+
+def ml_to_imperial_gallons(ml):
+    return ml / (4.54609 * 1000)
+
+def ml_to_imperial_quarts(ml):
+    return ml / (1.13652 * 1000)
+
+def ml_to_imperial_pints(ml):
+    return ml / (0.568261 * 1000)
+
+def ml_to_imperial_fl_oz(ml):
+    return ml / (0.0284131 * 1000)
+
+def l_to_imperial_cups(l):
+    return l / 0.284131
+
+def l_to_imperial_tablespoons(l):
+    return l / 0.0177582
+
+def l_to_imperial_teaspoons(l):
+    return l / 0.00591939
+
+def ml_to_imperial_cups(ml):
+    return ml / (0.284131 * 1000)
+
+def ml_to_imperial_tablespoons(ml):
+    return ml / (0.0177582 * 1000)
+
+def ml_to_imperial_teaspoons(ml):
+    return ml / (0.00591939 * 1000)
+
+
+# Metric to US
+
+def l_to_us_gallons(l):
+    return l / 3.78541
+
+def l_to_us_quarts(l):
+    return l / 0.946353
+
+def l_to_us_pints(l):
+    return l / 0.473176
+
+def l_to_us_fl_oz(l):
+    return l / 0.0295735
+
+def ml_to_us_gallons(ml):
+    return ml / (3.78541 * 1000)
+
+def ml_to_us_quarts(ml):
+    return ml / (0.946353 * 1000)
+
+def ml_to_us_pints(ml):
+    return ml / (0.473176 * 1000)
+
+def ml_to_us_fl_oz(ml):
+    return ml / (0.0295735 * 1000)
+
+def l_to_us_cups(l):
+    return l / 0.24
+
+def l_to_us_tablespoons(l):
+    return l / 0.0147868
+
+def l_to_us_teaspoons(l):
+    return l / 0.00492892
+
+def ml_to_us_cups(ml):
+    return ml / (0.24 * 1000)
+
+def ml_to_us_tablespoons(ml):
+    return ml / (0.0147868 * 1000)
+
+def ml_to_us_teaspoons(ml):
+    return ml / (0.00492892 * 1000)
+
+
+# Imperial to Metric
+
+def imperial_gallons_to_l(imp_gal):
+    return imp_gal * 4.54609
+
+def imperial_quarts_to_l(imp_qt):
+    return imp_qt * 1.13652
+
+def imperial_pints_to_l(imp_pt):
+    return imp_pt * 0.568261
+
+def imperial_fl_oz_to_l(imp_oz):
+    return imp_oz * 0.0284131
+
+def imperial_gallons_to_ml(imp_gal):
+    return imp_gal * 4.54609 * 1000
+
+def imperial_quarts_to_ml(imp_qt):
+    return imp_qt * 1.13652 * 1000
+
+def imperial_pints_to_ml(imp_pt):
+    return imp_pt * 0.568261 * 1000
+
+def imperial_fl_oz_to_ml(imp_oz):
+    return imp_oz * 0.0284131 * 1000
+
+def imperial_cups_to_l(imp_cups):
+    return imp_cups * 0.284131
+
+def imperial_tablespoons_to_l(imp_tbsp):
+    return imp_tbsp * 0.0177582
+
+def imperial_teaspoons_to_l(imp_tsp):
+    return imp_tsp * 0.00591939
+
+def imperial_cups_to_ml(imp_cups):
+    return imp_cups * 0.284131 * 1000
+
+def imperial_tablespoons_to_ml(imp_tbsp):
+    return imp_tbsp * 0.0177582 * 1000
+
+def imperial_teaspoons_to_ml(imp_tsp):
+    return imp_tsp * 0.00591939 * 1000
+
+
+# US to Metric 
+
+def us_gallons_to_l(us_gal):
+    return us_gal * 3.78541
+
+def us_quarts_to_l(us_qt):
+    return us_qt * 0.946353
+
+def us_pints_to_l(us_pt):
+    return us_pt * 0.473176
+
+def us_fl_oz_to_l(us_oz):
+    return us_oz * 0.0295735
+
+def us_gallons_to_ml(us_gal):
+    return us_gal * 3.78541 * 1000
+
+def us_quarts_to_ml(us_qt):
+    return us_qt * 0.946353 * 1000
+
+def us_pints_to_ml(us_pt):
+    return us_pt * 0.473176 * 1000
+
+def us_fl_oz_to_ml(us_oz):
+    return us_oz * 0.0295735 * 1000
+
+def us_cups_to_l(us_cups):
+    return us_cups * 0.24
+
+def us_tablespoons_to_l(us_tbsp):
+    return us_tbsp * 0.0147868
+
+def us_teaspoons_to_l(us_tsp):
+    return us_tsp * 0.00492892
+
+def us_cups_to_ml(us_cups):
+    return us_cups * 0.24 * 1000
+
+def us_tablespoons_to_ml(us_tbsp):
+    return us_tbsp * 0.0147868 * 1000
+
+def us_teaspoons_to_ml(us_tsp):
+    return us_tsp * 0.00492892 * 1000
+
+# Imperial to US
+
+def imperial_gallons_to_us_gallons(imp_gal):
+    return imp_gal * 1.20095
+
+def imperial_quarts_to_us_quarts(imp_qt):
+    return imp_qt * 1.20095
+
+def imperial_pints_to_us_pints(imp_pt):
+    return imp_pt * 1.20095
+
+def imperial_fl_oz_to_us_fl_oz(imp_oz):
+    return imp_oz * 1.04084
+
+def imperial_gallons_to_us_gallons(imp_gal):
+    return imp_gal * 1.20095
+
+def imperial_quarts_to_us_quarts(imp_qt):
+    return imp_qt * 1.20095
+
+def imperial_pints_to_us_pints(imp_pt):
+    return imp_pt * 1.20095
+
+def imperial_cups_to_us_cups(imp_cup):
+    return imp_cup * 1.20095
+
+def imperial_fl_oz_to_us_fl_oz(imp_oz):
+    return imp_oz * 1.04084
+
+def imperial_tablespoons_to_us_tablespoons(imp_tbsp):
+    return imp_tbsp * 1.20095
+
+def imperial_teaspoons_to_us_teaspoons(imp_tsp):
+    return imp_tsp * 1.20095
+
+
+# US to Imperial
+
+def us_gallons_to_imperial_gallons(us_gal):
+    return us_gal * 0.832674
+
+def us_quarts_to_imperial_quarts(us_qt):
+    return us_qt * 0.832674
+
+def us_pints_to_imperial_pints(us_pt):
+    return us_pt * 0.832674
+
+def us_fl_oz_to_imperial_fl_oz(us_oz):
+    return us_oz * 0.96076
+
+def us_gallons_to_imperial_gallons(us_gal):
+    return us_gal * 0.832674
+
+def us_quarts_to_imperial_quarts(us_qt):
+    return us_qt * 0.832674
+
+def us_pints_to_imperial_pints(us_pt):
+    return us_pt * 0.832674
+
+def us_cups_to_imperial_cups(us_cup):
+    return us_cup * 0.832674
+
+def us_fl_oz_to_imperial_fl_oz(us_oz):
+    return us_oz * 0.96076
+
+def us_tablespoons_to_imperial_tablespoons(us_tbsp):
+    return us_tbsp * 0.832674
+
+def us_teaspoons_to_imperial_teaspoons(us_tsp):
+    return us_tsp * 0.832674
+
+
+# US to US
+
+def us_gallons_to_us_quarts(us_gal):
+    return us_gal * 4
+
+def us_quarts_to_us_gallons(us_qt):
+    return us_qt / 4
+
+def us_quarts_to_us_pints(us_qt):
+    return us_qt * 2
+
+def us_pints_to_us_quarts(us_pt):
+    return us_pt / 2
+
+def us_pints_to_us_cups(us_pt):
+    return us_pt * 2
+
+def us_cups_to_us_pints(us_cup):
+    return us_cup / 2
+
+def us_cups_to_us_fl_oz(us_cup):
+    return us_cup * 8
+
+def us_fl_oz_to_us_cups(us_oz):
+    return us_oz / 8
+
+def us_fl_oz_to_us_tablespoons(us_oz):
+    return us_oz * 2
+
+def us_tablespoons_to_us_fl_oz(us_tbsp):
+    return us_tbsp / 2
+
+def us_tablespoons_to_us_teaspoons(us_tbsp):
+    return us_tbsp * 3
+
+def us_teaspoons_to_us_tablespoons(us_tsp):
+    return us_tsp / 3
+
+
+# Imperial to Imperial
+
+def imperial_gallons_to_imperial_quarts(imp_gal):
+    return imp_gal * 4
+
+def imperial_quarts_to_imperial_gallons(imp_qt):
+    return imp_qt / 4
+
+def imperial_quarts_to_imperial_pints(imp_qt):
+    return imp_qt * 2
+
+def imperial_pints_to_imperial_quarts(imp_pt):
+    return imp_pt / 2
+
+def imperial_pints_to_imperial_cups(imp_pt):
+    return imp_pt * 2
+
+def imperial_cups_to_imperial_pints(imp_cup):
+    return imp_cup / 2
+
+def imperial_cups_to_imperial_fl_oz(imp_cup):
+    return imp_cup * 10
+
+def imperial_fl_oz_to_imperial_cups(imp_oz):
+    return imp_oz / 10
+
+def imperial_fl_oz_to_imperial_tablespoons(imp_oz):
+    return imp_oz * 1.6
+
+def imperial_tablespoons_to_imperial_fl_oz(imp_tbsp):
+    return imp_tbsp / 1.6
+
+def imperial_tablespoons_to_imperial_teaspoons(imp_tbsp):
+    return imp_tbsp * 3
+
+def imperial_teaspoons_to_imperial_tablespoons(imp_tsp):
+    return imp_tsp / 3
